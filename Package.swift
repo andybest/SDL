54af9a86f7353774e54924b7738c56f566a91388
Andy Best <Andy.Best@tribalworldwide.co.uk>
2017-06-13 12:34:03 +0100

    Updated package to point to temp repo

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SDLDemo"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/PureSwift/CSDL2.git", .branch("master"))
+        .package(url: "https://github.com/andybest/CSDL2.git", from: "1.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
